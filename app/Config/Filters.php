ec9ae470c59dc5545475c9f8eab057de1041770b
Venkadesh-muthu <venkatesh@srivatech.com>
2026-07-19 15:45:01 +0530

    removed codeigniter icon

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -5,7 +5,6 @@ namespace Config;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\ForceHTTPS;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
@@ -26,7 +25,6 @@ class Filters extends BaseFilters
      */
     public array $aliases = [
         'csrf' => CSRF::class,
-        'toolbar' => DebugToolbar::class,
         'honeypot' => Honeypot::class,
         'invalidchars' => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
@@ -58,7 +56,6 @@ class Filters extends BaseFilters
         'after' => [
             'pagecache',   // Web Page Caching
             'performance', // Performance Metrics
-            'toolbar',     // Debug Toolbar
         ],
     ];
 
